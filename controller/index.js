68ae4ebe30a6c0fdd34421ef3fcd7dcb2b65bf39
Aaron <aaron.m.pelzer@gmail.com>
2018-01-28 22:14:03 -0500

    Config tweaks

--- controller/index.js
+++ controller/index.js
@@ -2,16 +2,18 @@ var express = require("express"),
     router = express(),
     uploadUtil = require('../util/upload.js');
 
+
 router.get("/", function(req, res) {
     if(!req.user){
         res.redirect('account/login');
+    } else {
+        res.render("main/index", {
+            title: "Index"
+        });
     }
 
-
-    res.render("main/index", {
-        title: "Index"
-    });
 });
+
 
 router.get("/contact", function(req, res) {
     res.render("main/contact", {
